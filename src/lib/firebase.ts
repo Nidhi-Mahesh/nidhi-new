6d85d589243554e1928260f215030d9feb2e0a37
Inchara P <pinchara01@gmail.com>
2025-09-04 06:53:13 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -7,12 +8,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  "apiKey": "AIzaSyCkQa4_b9KfqzHVPF9ARc40ty7UjUlU2Sg",
+  "apiKey": "AIzaSyCak-4DjsKp9DE6NWu0wQQyV4k8g6_XgiY",
   "authDomain": "modern-chyrp.firebaseapp.com",
   "projectId": "modern-chyrp",
   "storageBucket": "modern-chyrp.appspot.com",
   "messagingSenderId": "380240154321",
-  "appId": "1:380240154321:web:198f17a57c8cc1044dfff0"
+  "appId": "1:380240154321:web:82d1063fa55e45cc4dfff0"
 };
 
 // Initialize Firebase
